b9ad62d9a825e9927d01f827b203e58b50139e5d
Larissa <larissa_rodr@hotmail.com>
2020-06-09 20:20:04 -0500

    Changing migrations

--- src/database/migrations/1591743578302-CreateAppointments.ts
+++ src/database/migrations/1591743578302-CreateAppointments.ts
@@ -9,14 +9,15 @@ export default class CreateAppointments1591743578302
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
-            type: 'varchar',
+            name: 'provider_id',
+            type: 'uuid',
+            isNullable: true,
           },
           {
             name: 'date',
---
+++ src/database/migrations/1591751560486-CreateFKProviderUser.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class CreateFKProviderUser1591751560486
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'fk_provider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'fk_provider');
+  }
+}
